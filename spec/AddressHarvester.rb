61aa70125db949b2a0350a4d2179b52d3325e5d2
Andrey Brindeyev <abrindeyev@griddynamics.com>
2013-02-01 01:37:42 -0800

    renamed tests

--- spec/AddressHarvester.rb
+++ spec/AddressHarvester.rb
@@ -7,32 +7,32 @@ describe "Address harvester object" do
   it { should_not eq(nil) }
 end
 
-describe AddressHarvester, "Address harvester object" do
+describe "Get xstreet0 tag value" do
   subject { AddressHarvester.new(File.join(samples, '3574423811.html')).get_tag('xstreet0') }
   it { should == '120 Granada' }
 end
 
-describe AddressHarvester, "Address harvester object" do
+describe "Get city tag value" do
   subject { AddressHarvester.new(File.join(samples, '3574423811.html')).get_tag('city') }
   it { should == 'Mountain View' }
 end
 
-describe AddressHarvester, "Address harvester object" do
   it { should == 'CA' }
+describe "Get region tag value" do
   subject { AddressHarvester.new(File.join(samples, '3574423811.html')).get_tag('region') }
 end
 
-describe AddressHarvester, "Address harvester object" do
+describe "Missing region tag" do
   subject { AddressHarvester.new(File.join(samples, '3574419831.html')).get_tag('region') }
   it { should_not == 'CA' }
 end
 
-describe AddressHarvester, "Address harvester object" do
+describe "Posting with full address from internal pattern database" do
   subject { AddressHarvester.new(File.join(samples, '3573633080.html')).have_full_address? }
   it { should == true }
 end
 
-describe AddressHarvester, "Address harvester object" do
+describe "Get full address from matched database entry" do
   subject { AddressHarvester.new(File.join(samples, '3573633080.html')).get_full_address }
   it { should == '4022 Papillon Terrace, FREMONT , CA' }
 end
