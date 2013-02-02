3202e6a1e34efcaed85ef6c06e2a69d808b4c4c1
Andrey Brindeyev <abrindeyev@griddynamics.com>
2013-02-02 02:41:08 -0800

    Make BART distance calculations only if city is Fremont

--- check_address.rb
+++ check_address.rb
@@ -27,32 +27,34 @@ puts "Report for address: #{geo['results'][0]['formatted_address']}"
 a_lat = geo['results'][0]['geometry']['location']['lat']
 a_lng = geo['results'][0]['geometry']['location']['lng']
 
-s = SchoolMatcher.new(a_lat, a_lng)
-post.set_feature(:school_name, s.get_school_name)
-post.set_feature(:school_rating, s.get_rating)
-post.set_feature(:school_addr, s.get_school_address)
-directions_url = "http://maps.googleapis.com/maps/api/directions/json?origin=#{ URI.escape(addr) }&destination=#{ URI.escape(s.get_school_address) }&sensor=false&mode=driving"
-resp = RestClient.get(directions_url)
-geo2 = JSON.parse(resp.body)
-unless geo2['status'] == 'OK'
-  puts "Directions failed: #{geo['status']}"
-  exit -1
-end
-puts "Driving to school by car is #{geo2['routes'][0]['legs'][0]['duration']['text']}"
+if post.get_city.match(/fremont/i)
+  s = SchoolMatcher.new(a_lat, a_lng)
+  post.set_feature(:school_name, s.get_school_name)
+  post.set_feature(:school_rating, s.get_rating)
+  post.set_feature(:school_addr, s.get_school_address)
+  directions_url = "http://maps.googleapis.com/maps/api/directions/json?origin=#{ URI.escape(addr) }&destination=#{ URI.escape(s.get_school_address) }&sensor=false&mode=driving"
+  resp = RestClient.get(directions_url)
+  geo2 = JSON.parse(resp.body)
+  unless geo2['status'] == 'OK'
+    puts "Directions failed: #{geo['status']}"
+    exit -1
+  end
+  puts "Driving to school by car is #{geo2['routes'][0]['legs'][0]['duration']['text']}"
 
-n = ''
-#puts geo.inspect
-geo['results'][0]['address_components'].each {|h| n = h['short_name'] if h['types'][0] == 'neighborhood' }
-#puts "Neighborhood: #{n}"
-post.set_feature(:neighborhood, n)
+  n = ''
+  #puts geo.inspect
+  geo['results'][0]['address_components'].each {|h| n = h['short_name'] if h['types'][0] == 'neighborhood' }
+  #puts "Neighborhood: #{n}"
+  post.set_feature(:neighborhood, n)
 
-directions_url = "http://maps.googleapis.com/maps/api/directions/json?origin=#{ URI.escape(addr) }&destination=Fremont+BART+station&sensor=false&mode=walking"
-resp = RestClient.get(directions_url)
-geo2 = JSON.parse(resp.body)
-unless geo2['status'] == 'OK'
-  puts "Directions failed: #{geo2['status']}"
-  exit -1
+  directions_url = "http://maps.googleapis.com/maps/api/directions/json?origin=#{ URI.escape(addr) }&destination=Fremont+BART+station&sensor=false&mode=walking"
+  resp = RestClient.get(directions_url)
+  geo2 = JSON.parse(resp.body)
+  unless geo2['status'] == 'OK'
+    puts "Directions failed: #{geo2['status']}"
+    exit -1
+  end
+  puts "Walking to Fremont BART: #{geo2['routes'][0]['legs'][0]['duration']['text']}"
 end
-puts "Walking to Fremont BART: #{geo2['routes'][0]['legs'][0]['duration']['text']}"
 puts post.get_features.inspect
 puts "Posting score: " + post.get_score.to_s
