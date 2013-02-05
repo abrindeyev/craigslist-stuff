0f6eaf7233a3f70704e7f23391bd1c17aab1c3cd
Andrey Brindeyev <abrindeyev@griddynamics.com>
2013-02-05 10:27:31 -0800

    Added exception handling to batch processing

--- get_list.rb
+++ get_list.rb
@@ -80,6 +80,11 @@ else
     full_link = "http://#{external_ip}/#{filename}"
     short_link = open("http://clck.ru/--?url="+full_link).read
     puts short_link
-    Twitter.update("[#{post.get_score}] $#{post.get_feature(:rent_price)} " + (post.have_feature?(:name) ? post.get_feature(:name) : post.get_full_address) + " #{short_link}")
+    begin
+      tweet = "[#{post.get_score}] $#{post.get_feature(:rent_price)} " + (post.have_feature?(:name) ? post.get_feature(:name) : post.get_full_address) + " #{short_link}"
+      Twitter.update(tweet)
+    rescue Twitter::Error::Forbidden
+      puts "Caught an exception during posting following tweet: [#{tweet}]: #{$!}"
+    end
   end
 end
