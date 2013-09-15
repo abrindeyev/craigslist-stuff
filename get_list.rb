7f4bd25bc3db1daf0f6f27abc04206b1896b59e6
Andrey Brindeyev <abrindeyev@griddynamics.com>
2013-09-15 13:37:50 -0700

    Adjusted maximum asked rent price to $2500

--- get_list.rb
+++ get_list.rb
@@ -13,7 +13,7 @@ require './lib/school'
 
 STDOUT.sync = true
 
-source_url = 'http://sfbay.craigslist.org/search/apa/eby?zoomToPosting=&altView=&query=&srchType=A&minAsk=&maxAsk=2200&bedrooms=2&nh=54'
+source_url = 'http://sfbay.craigslist.org/search/apa/eby?zoomToPosting=&altView=&query=&srchType=A&minAsk=&maxAsk=2500&bedrooms=2&nh=54'
 page = Nokogiri::HTML(open(source_url).read, nil, 'UTF-8')
 links = page.xpath("//body/article[@id='pagecontainer']/section[@class='body']/blockquote[@id='toc_rows']/p[@class='row']/a[@href]")
 o = YAML.load_file('.settings.yaml')
